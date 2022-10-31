d16524f0300f9bd3c2f19040f6c78201091d1260
Svetlana Bushmakina <svetik_5073@mail.ru>
2022-11-01 00:20:10 +0400

    Getter and Setter

---
+++ src/main/java/Wizard.java
@@ -0,0 +1,12 @@
+public class Wizard {
+    private int powerOfMagic = 100500;
+
+    public void setPowerOfMagic(int powerOfMagic) {
+        this.powerOfMagic = powerOfMagic;
+    }
+
+    public int getPowerOfMagic() {
+        return powerOfMagic;
+    }
+}
+
